d111288be9d20d92f860e8648d00ea1d952c2bd1
napzalav <cesaralavila@hotmail.com>
2024-03-22 19:33:42 -0300

    conexion controlador-modelo

--- controllers/principal/Principal.php
+++ controllers/principal/Principal.php
@@ -1,7 +1,14 @@
 <?php
-class Principal{
+//extendemos desde la clase Controller en la carpeta app para realizar una herencia
+//para cargar la herencia necesitamos generar un constructor y adentro cargar el modelo mediante "parent"
+class Principal extends Controller{
+    public function __construct(){
+        parent::__construct();
+    }
     public function index() {
-        echo 'Mensaje desde el controlador';
+        $data = $this->model->getPrueba();
+        echo $data;
+        // echo 'Mensaje desde el controlador';
     }
 
 }
---
+++ models/principal/PrincipalModel.php
@@ -0,0 +1,20 @@
+<?php
+class PrincipalModel{
+    public function getPrueba(){
+        echo 'Mensaje desde el modelo';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
