84a5642d8342b6470350c1ab5707c611fab51fa5
napzalav <cesaralavila@hotmail.com>
2024-03-28 13:07:29 -0300

    metodos en Query

--- config/app/Query.php
+++ config/app/Query.php
@@ -6,8 +6,42 @@ class Query extends Conexion{
         $this->pdo = $this->con->conectar();
     }
 
-    public function select(){
+    //RECUPERAR UN SOLO REGISTRO
+    public function select($sql){
+        $result = $this->pdo->prepare($sql);
+        $result->execute();
+        return $result->fetch(PDO::FETCH_ASSOC);
+    }
 
+    //RECUPERAR TODOS LOS REGISTROS
+    public function selectAll($sql){
+        $result = $this->pdo->prepare($sql);
+        $result->execute();
+        return $result->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    //REGISTRAR
+    public function insert($sql, $array){
+        $result = $this->pdo->prepare($sql);
+        $data = $result->execute($array);
+        if ($data) {
+            $res = $this->pdo->lastInsertId();
+        }else{
+            $res = 0;
+        }
+        return $res;
+    }
+
+    //MODIFICAR O ELIMINAR
+    public function save($sql, $array){
+        $result = $this->pdo->prepare($sql);
+        $data = $result->execute($array);
+        if ($data) {
+            $res = 1;
+        }else{
+            $res = 0;
+        }
+        return $res;
     }
 
 }
