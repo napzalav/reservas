56932a64b6fa31309dd5e461c6d2d7c679fab2a9
napzalav <cesaralavila@hotmail.com>
2024-03-28 14:13:15 -0300

    Helpers - capturar nombre del dia

--- helpers/funciones.php
+++ helpers/funciones.php
@@ -289,3 +289,33 @@ function generate_numbers($start, $count, $digits)
     return $result;
 }
 
+//CAPTURAR EL NOMBRE DEL DIA
+function get_nombre_dia($fecha)
+{
+    $fechats = strtotime($fecha); //pasamos a timestamp
+
+    //lo devuelve en numero 0 domingo, 1 lunes,....
+    switch (date('w', $fechats)) {
+        case 0:
+            return "Domingo";
+            break;
+        case 1:
+            return "Lunes";
+            break;
+        case 2:
+            return "Martes";
+            break;
+        case 3:
+            return "Miercoles";
+            break;
+        case 4:
+            return "Jueves";
+            break;
+        case 5:
+            return "Viernes";
+            break;
+        case 6:
+            return "Sabado";
+            break;
+    }
+}
