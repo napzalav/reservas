5250b02685e98b4948c3bb90a45af14060288398
napzalav <cesaralavila@hotmail.com>
2024-03-28 13:37:18 -0300

    Helpers-personalizar fecha

--- helpers/funciones.php
+++ helpers/funciones.php
@@ -72,6 +72,30 @@ function limitar_cadena($cadena, $limite, $sufijo)
     return $cadena;
 }
 
+//PERSONALIZAR FECHA
+function fechaPerzo($fecha)
+{
+    $datos = explode('-', $fecha);
+    $anio = $datos[0];
+    $me = ltrim($datos[1], "0");
+    $dia = $datos[2];
+    $mes = array(
+        "",
+        "Enero",
+        "Febrero",
+        "Marzo",
+        "Abril",
+        "Mayo",
+        "Junio",
+        "Julio",
+        "Agosto",
+        "Septiembre",
+        "Octubre",
+        "Noviembre",
+        "Diciembre"
+    );
+    return $dia . " de " . $mes[$me] . " de " . $anio;
+}
 
 
 
