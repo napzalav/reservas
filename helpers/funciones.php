2084638c225315ee4cc4108f5c693857974f1954
napzalav <cesaralavila@hotmail.com>
2024-03-28 13:32:40 -0300

    Helpers- limpiar caracteres especiales-slug-limitar cadena

---
+++ helpers/funciones.php
@@ -0,0 +1,79 @@
+<?php
+//LIMPIAR CARACTERES ESPECIALES PARA PREVENIR INYECCION SQL
+function strClean($cadena)
+{
+    $string = preg_replace(['/\s+/', '/^\s|\s$/'], [' ', ''], $cadena);
+    $string = trim($string);
+    $string = stripslashes($string);
+    $string = str_ireplace('<script>', '', $string);
+    $string = str_ireplace('</script>', '', $string);
+    $string = str_ireplace('<script type=>', '', $string);
+    $string = str_ireplace('<script src>', '', $string);
+    $string = str_ireplace('SELECT * FROM', '', $string);
+    $string = str_ireplace('DELETE FROM', '', $string);
+    $string = str_ireplace('INSERT INTO', '', $string);
+    $string = str_ireplace('SELECT COUNT(*) FROM', '', $string);
+    $string = str_ireplace('DROP TABLE', '', $string);
+    $string = str_ireplace("OR '1'='1", '', $string);
+    $string = str_ireplace('OR ´1´=´1', '', $string);
+    $string = str_ireplace('IS NULL', '', $string);
+    $string = str_ireplace('LIKE "', '', $string);
+    $string = str_ireplace("LIKE '", '', $string);
+    $string = str_ireplace('LIKE ´', '', $string);
+    $string = str_ireplace('OR "a"="a', '', $string);
+    $string = str_ireplace("OR 'a'='a", '', $string);
+    $string = str_ireplace('OR ´a´=´a', '', $string);
+    $string = str_ireplace('--', '', $string);
+    $string = str_ireplace('^', '', $string);
+    $string = str_ireplace('[', '', $string);
+    $string = str_ireplace(']', '', $string);
+    $string = str_ireplace('==', '', $string);
+    return $string;
+}
+
+//CREAR SLUG (un SLUG es una pequeña parte de un enlace que permite identificar un contenido especifico en una web)
+function slugify($text, string $divider = '-')
+{
+    // replace non letter or digits by divider
+    $text = preg_replace('~[^\pL\d]+~u', $divider, $text);
+
+    // transliterate
+    $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+    // remove unwanted characters
+    $text = preg_replace('~[^-\w]+~', '', $text);
+
+    // trim
+    $text = trim($text, $divider);
+
+    // remove duplicate divider
+    $text = preg_replace('~-+~', $divider, $text);
+
+    // lowercase
+    $text = strtolower($text);
+
+    if (empty($text)) {
+        return 'n-a';
+    }
+
+    return $text;
+}
+
+//LIMITAR CADENA (limitamos la cantidad de caracteres que necesitamos mostrar)
+function limitar_cadena($cadena, $limite, $sufijo)
+{
+    // Si la longitud es mayor que el límite...
+    if (strlen($cadena) > $limite) {
+        // Entonces corta la cadena y ponle el sufijo
+        return substr($cadena, 0, $limite) . $sufijo;
+    }
+
+    // Si no, entonces devuelve la cadena normal
+    return $cadena;
+}
+
+
+
+
+
+?>
