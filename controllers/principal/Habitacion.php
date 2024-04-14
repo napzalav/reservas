67ebb35acd74102436f609be04473348657f0a21
napzalav <cesaralavila@hotmail.com>
2024-04-14 16:57:26 -0300

    add controller Habitacion

---
+++ controllers/principal/Habitacion.php
@@ -0,0 +1,12 @@
+<?php
+class Habitacion extends Controller{
+    public function __construct(){
+        parent::__construct();
+    }
+    public function index() {
+        $data['title'] = 'Habitaciones';
+        $this->views->getView('index', $data);
+    }
+}
+
+?>
