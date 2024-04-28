6ce8803cda71008c8bbd7a3cf28c9c5cf4ef4784
napzalav <cesaralavila@hotmail.com>
2024-04-28 14:06:14 -0300

    add controller blog

---
+++ controllers/principal/Blog.php
@@ -0,0 +1,12 @@
+<?php
+class Blog extends Controller{
+    public function __construct(){
+        parent::__construct();
+    }
+    public function index() {
+        $data['title'] = 'Blog';
+        $this->views->getView('principal/blog/index', $data);
+    }
+}
+
+?>
