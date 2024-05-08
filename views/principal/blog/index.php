2898f6a0dd44557c04dbc8baceea42c18664b060
napzalav <cesaralavila@hotmail.com>
2024-05-08 13:23:23 -0300

    update view blog

--- views/principal/blog/index.php
+++ views/principal/blog/index.php
@@ -11,100 +11,259 @@
                         Home
                     </a>
                 </li>
-                <li>Room</li>
-                <li>Room Style Two</li>
+                <li>Noticias</li>
+                <li>Nuevos articulos</li>
             </ul>
         </div>
     </div>
 </div>
 <!-- End Page Title Area -->
 
-<section class="our-rooms-area-two our-rooms-area-four ptb-100">
+<!-- End News Area -->
+<section class="news-area ptb-100">
     <div class="container">
         <div class="section-title">
-            <span>Nuestras habitaciones</span>
-            <h2>Fascinating rooms & suites</h2>
+            <span>Our BLog</span>
+            <h2>News & articles updates </h2>
         </div>
-        <div class="tab industries-list-tab">
-            <div class="row">
-                <div class="col-lg-5">
-                    <div class="tabs row">
-                        <div class="col-lg-6 col-sm-6 single-tab">
-                            <div class="single-rooms">
-                                <i class="flaticon-online-booking"></i>
-                                <span class="booking-title">Free cost</span>
-                                <h3>No booking fee</h3>
-                            </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6">
+                <div class="single-news">
+                    <div class="news-img">
+                        <a href="news-details.html">
+                            <img src="<?php echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/news/1.jpg" alt="Image">
+                        </a>
+                        <div class="dates">
+                            <span>HOTEL</span>
                         </div>
-                        <div class="col-lg-6 col-sm-6 single-tab">
-                            <div class="single-rooms">
-                                <i class="flaticon-podium"></i>
-                                <span class="booking-title">Free cost</span>
-                                <h3>Best rate guarantee</h3>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-sm-6 single-tab">
-                            <div class="single-rooms">
-                                <i class="flaticon-airport"></i>
-                                <span class="booking-title">Free cost</span>
-                                <h3>Reservations 24/7</h3>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-sm-6 single-tab">
-                            <div class="single-rooms">
-                                <i class="flaticon-slow"></i>
-                                <span class="booking-title">Free cost</span>
-                                <h3>High-speed Wi-Fi</h3>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-sm-6 single-tab">
-                            <div class="single-rooms">
-                                <i class="flaticon-coffee-cup-1"></i>
-                                <span class="booking-title">Free cost</span>
-                                <h3>Free breakfast</h3>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-sm-6 single-tab">
-                            <div class="single-rooms">
-                                <i class="flaticon-user"></i>
-                                <span class="booking-title">100% free</span>
-                                <h3>One person free</h3>
-                            </div>
-                        </div>
+                    </div>
+                    <div class="news-content-wrap">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <i class="flaticon-user"></i>
+                                    Admin
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="flaticon-conversation"></i>
+                                    Comment
+                                </a>
+                            </li>
+                        </ul>
+                        <a href="news-details.html">
+                            <h3>Celebrating Decade Years Of Hotel In 2020</h3>
+                        </a>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga veritatis veniam corrupti perferendis.</p>
+                        <a class="read-more" href="news-details.html">
+                            Read More
+                            <i class="flaticon-right"></i>
+                        </a>
                     </div>
                 </div>
-                <div class="col-lg-7">
-                    <div class="tab_content">
-                        <div class="tabs_item">
-                            <div class="our-rooms-single-img room-bg-1">
-                            </div>
-                        </div>
-                        <div class="tabs_item">
-                            <div class="our-rooms-single-img room-bg-2">
-                            </div>
-                        </div>
-                        <div class="tabs_item">
-                            <div class="our-rooms-single-img room-bg-3">
-                            </div>
-                        </div>
-                        <div class="tabs_item">
-                            <div class="our-rooms-single-img room-bg-4">
-                            </div>
-                        </div>
-                        <div class="tabs_item">
-                            <div class="our-rooms-single-img room-bg-5">
-                            </div>
-                        </div>
-                        <div class="tabs_item">
-                            <div class="our-rooms-single-img room-bg-6">
-                            </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="single-news">
+                    <div class="news-img">
+                        <a href="news-details.html">
+                            <img src="<?php echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/news/2.jpg" alt="Image">
+                        </a>
+                        <div class="dates">
+                            <span>PRICE</span>
                         </div>
                     </div>
+                    <div class="news-content-wrap">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <i class="flaticon-user"></i>
+                                    Admin
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="flaticon-conversation"></i>
+                                    Comment
+                                </a>
+                            </li>
+                        </ul>
+                        <a href="news-details.html">
+                            <h3>A Perfect Day With Businessman At Ecorik Hotel</h3>
+                        </a>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga veritatis veniam corrupti perferendis.</p>
+                        <a class="read-more" href="news-details.html">
+                            Read More
+                            <i class="flaticon-right"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="single-news">
+                    <div class="news-img">
+                        <a href="news-details.html">
+                            <img src="<?php echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/news/1.jpg" alt="Image">
+                        </a>
+                        <div class="dates">
+                            <span>STORE</span>
+                        </div>
+                    </div>
+                    <div class="news-content-wrap">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <i class="flaticon-user"></i>
+                                    Admin
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="flaticon-conversation"></i>
+                                    Comment
+                                </a>
+                            </li>
+                        </ul>
+                        <a href="news-details.html">
+                            <h3>Celebrating Decade Years Of Hotel In 2019</h3>
+                        </a>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga veritatis veniam corrupti perferendis.</p>
+                        <a class="read-more" href="news-details.html">
+                            Read More
+                            <i class="flaticon-right"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="single-news">
+                    <div class="news-img">
+                        <a href="news-details.html">
+                            <img src="<?php echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/news/11.jpg" alt="Image">
+                        </a>
+                    </div>
+                    <div class="news-content-wrap">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <i class="flaticon-user"></i>
+                                    Admin
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="flaticon-conversation"></i>
+                                    Comment
+                                </a>
+                            </li>
+                        </ul>
+                        <a href="news-details.html">
+                            <h3>Ecorik Hotel Family Party</h3>
+                        </a>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga veritatis veniam corrupti perferendis.</p>
+                        <a class="read-more" href="news-details.html">
+                            Read More
+                            <i class="flaticon-right"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="single-news">
+                    <div class="news-img">
+                        <a href="news-details.html">
+                            <img src="<?php echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/news/12.jpg" alt="Image">
+                        </a>
+                    </div>
+                    <div class="news-content-wrap">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <i class="flaticon-user"></i>
+                                    Admin
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="flaticon-conversation"></i>
+                                    Comment
+                                </a>
+                            </li>
+                        </ul>
+                        <a href="news-details.html">
+                            <h3>Ecorik Hotel In Pictures</h3>
+                        </a>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga veritatis veniam corrupti perferendis.</p>
+                        <a class="read-more" href="news-details.html">
+                            Read More
+                            <i class="flaticon-right"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="single-news">
+                    <div class="news-img">
+                        <a href="news-details.html">
+                            <img src="<?php echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/news/13.jpg" alt="Image">
+                        </a>
+                    </div>
+                    <div class="news-content-wrap">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <i class="flaticon-user"></i>
+                                    Admin
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="flaticon-conversation"></i>
+                                    Comment
+                                </a>
+                            </li>
+                        </ul>
+                        <a href="news-details.html">
+                            <h3>Live Your Myth In Greece</h3>
+                        </a>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga veritatis veniam corrupti perferendis.</p>
+                        <a class="read-more" href="news-details.html">
+                            Read More
+                            <i class="flaticon-right"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-12">
+                <div class="page-navigation-area">
+                    <nav aria-label="Page navigation example text-center">
+                        <ul class="pagination">
+                            <li class="page-item">
+                                <a class="page-link page-links" href="#">
+                                    <i class='bx bx-chevrons-left'></i>
+                                </a>
+                            </li>
+                            <li class="page-item active">
+                                <a class="page-link" href="#">1</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">2</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">3</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">
+                                    <i class='bx bx-chevrons-right'></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
             </div>
         </div>
     </div>
 </section>
-
+<!-- End News Area -->
 
 <?php include_once 'views/template/footer-principal.php'; ?>
