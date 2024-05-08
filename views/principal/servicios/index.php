ce5c04245a0b5469709046bf9756ab50c0e6f4e1
napzalav <cesaralavila@hotmail.com>
2024-05-08 13:07:16 -0300

    update view servicios

--- views/principal/servicios/index.php
+++ views/principal/servicios/index.php
@@ -19,6 +19,274 @@
 </div>
 <!-- End Page Title Area -->
 
+<!-- Start Explore Area -->
+<section class="explore-area ptb-100">
+    <div class="container">
+        <div class="section-title">
+            <span>Explore</span>
+            <h2>We are cool to give you pleasure</h2>
+        </div>
+        <div class="row align-items-center">
+            <div class="col-lg-6">
+                <div class="explore-img">
+                    <img src="<?php echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/explore-img.png" alt="Image">
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="explore-content ml-30">
+                    <h2>As much as comfort want to get from us everything</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt eveniet reprehenderit ratione ad perspiciatis repudiandae iste ipsam temporibus sit quo! Incidunt, necessitatibus fugiat ut dignissimos pariatur odit natus ipsum! Obcaecati iste ipsam temporibus sit quo! Incidunt, necessitatibus Obcaecati iste ipsam temporibus.</p>
 
+                    <p>Konin wansis dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut nim ad minim veniam, quis nostrud exercitation. dolor sit amet, consectetur adipisicing quis nostrud exercitation.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Explore Area -->
+
+<!-- Start Our Rooms Area -->
+<section class="our-rooms-area pb-100">
+    <div class="container">
+        <div class="section-title">
+            <span>Our Rooms</span>
+            <h2>Fascinating rooms & suites</h2>
+        </div>
+        <div class="tab industries-list-tab">
+            <div class="row">
+                <div class="col-lg-4">
+                    <ul class="tabs">
+                        <li class="single-rooms">
+                            <img src="<?php echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/rooms/button-img-1.jpg" alt="Image">
+                            <div class="room-content">
+                                <h3>Double Room</h3>
+                                <span>From $75.9/night</span>
+                            </div>
+                        </li>
+                        <li class="single-rooms">
+                            <img src="<?php echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/rooms/button-img-2.jpg" alt="Image">
+                            <div class="room-content">
+                                <h3>Luxury Room</h3>
+                                <span>From $50.9/night</span>
+                            </div>
+                        </li>
+                        <li class="single-rooms">
+                            <img src="<?php echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/rooms/button-img-3.jpg" alt="Image">
+                            <div class="room-content">
+                                <h3>Best Room</h3>
+                                <span>From $70.9/night</span>
+                            </div>
+                        </li>
+                        <li class="single-rooms">
+                            <img src="<?php echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/rooms/button-img-4.jpg" alt="Image">
+                            <div class="room-content">
+                                <h3>Classic Room</h3>
+                                <span>From $95.9/night</span>
+                            </div>
+                        </li>
+                        <li class="single-rooms">
+                            <img src="<?php echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/rooms/button-img-5.jpg" alt="Image">
+                            <div class="room-content">
+                                <h3>Budget Room</h3>
+                                <span>From $95.9/night</span>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-8">
+                    <div class="tab_content">
+                        <div class="tabs_item">
+                            <div class="our-rooms-single-img room-bg-1">
+                            </div>
+                            <span class="preview-item">The Preview Of Double Room</span>
+                        </div>
+                        <div class="tabs_item">
+                            <div class="our-rooms-single-img room-bg-2">
+                            </div>
+                            <span class="preview-item">The Preview Of Luxury Room</span>
+                        </div>
+                        <div class="tabs_item">
+                            <div class="our-rooms-single-img room-bg-3">
+                            </div>
+                            <span class="preview-item">The Preview Of Best Room</span>
+                        </div>
+                        <div class="tabs_item">
+                            <div class="our-rooms-single-img room-bg-4">
+                            </div>
+                            <span class="preview-item">The Preview Of Classic Room</span>
+                        </div>
+                        <div class="tabs_item">
+                            <div class="our-rooms-single-img room-bg-5">
+                            </div>
+                            <span class="preview-item">The Preview Of Budget Room</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Our Rooms Area -->
+
+<!-- Start City View Area -->
+<section class="city-view-area ptb-100">
+    <div class="container">
+        <div class="city-wrap">
+            <div class="single-city-item owl-carousel owl-theme">
+                <div class="city-view-single-item">
+                    <div class="city-content">
+                        <span>The City View</span>
+                        <h3>A charming view of the city town</h3>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur necessitatibus fugit eligendi accusantium vel quos debitis cupiditate ducimus placeat explicabo distinctio, consectetur eos animi, a voluptate delectus. Id, explicabo saepe Consequuntur</p>
+
+                        <p>The view onin wansis dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ad minim veniam, quis nostrud exercitation consectetur.</p>
+                    </div>
+                </div>
+                <div class="city-view-single-item">
+                    <div class="city-content">
+                        <span>The City View</span>
+                        <h3>The charming view of the city</h3>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur necessitatibus fugit eligendi accusantium vel quos debitis cupiditate ducimus placeat explicabo distinctio, consectetur eos animi, a voluptate delectus. Id, explicabo saepe Consequuntur</p>
+
+                        <p>The view onin wansis dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ad minim veniam, quis nostrud exercitation consectetur.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End City View Area -->
+
+<!-- Start Counter Area -->
+<section class="counter-area pt-100 pb-70 jarallax">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-sm-6">
+                <div class="single-counter">
+                    <p>Beaches</p>
+                    <h2>
+                        <span class="odometer" data-count="50">00</span> <span class="target">+</span>
+                    </h2>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="single-counter">
+                    <p>Spa offers</p>
+                    <h2>
+                        <span class="odometer" data-count="95">00</span> <span class="target">+</span>
+                    </h2>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="single-counter">
+                    <p>Rooms</p>
+                    <h2>
+                        <span class="odometer" data-count="45">00</span> <span class="target">+</span>
+                    </h2>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="single-counter">
+                    <p>Happy client</p>
+                    <h2>
+                        <span class="odometer" data-count="20">00</span> <span class="target">K</span>
+                    </h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Counter Area -->
+
+<!-- start Testimonials Area -->
+<section class="testimonials-area ptb-100">
+    <div class="container">
+        <div class="section-title">
+            <span>Testimonials</span>
+            <h2>What customers say</h2>
+        </div>
+        <div class="testimonials-wrap owl-carousel owl-theme">
+            <div class="single-testimonials">
+                <ul>
+                    <li>
+                        <i class="bx bxs-star"></i>
+                    </li>
+                    <li>
+                        <i class="bx bxs-star"></i>
+                    </li>
+                    <li>
+                        <i class="bx bxs-star"></i>
+                    </li>
+                    <li>
+                        <i class="bx bxs-star"></i>
+                    </li>
+                    <li>
+                        <i class="bx bxs-star"></i>
+                    </li>
+                </ul>
+                <h3>Excellent hotel</h3>
+                <p>“Awesome yksum dolor sit ametco elit, sed do eiusmod tempor incididunt et md do eiusmoeiusmod tempor inte emamnsecacing eiusmoeiusmod”</p>
+                <div class="testimonials-content">
+                    <img src="<?php echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/testimonials/2.jpg" alt="Image">
+                    <h4>Ayman Jenis</h4>
+                    <span>CEO@Leasuely</span>
+                </div>
+            </div>
+            <div class="single-testimonials">
+                <ul>
+                    <li>
+                        <i class="bx bxs-star"></i>
+                    </li>
+                    <li>
+                        <i class="bx bxs-star"></i>
+                    </li>
+                    <li>
+                        <i class="bx bxs-star"></i>
+                    </li>
+                    <li>
+                        <i class="bx bxs-star"></i>
+                    </li>
+                    <li>
+                        <i class="bx bxs-star"></i>
+                    </li>
+                </ul>
+                <h3>Excellent hotel</h3>
+                <p>“Awesome yksum dolor sit ametco elit, sed do eiusmod tempor incididunt et md do eiusmoeiusmod tempor inte emamnsecacing eiusmoeiusmod”</p>
+                <div class="testimonials-content">
+                    <img src="<?php echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/testimonials/3.jpg" alt="Image">
+                    <h4>Ayman Jenis</h4>
+                    <span>CEO@Leasuely</span>
+                </div>
+            </div>
+            <div class="single-testimonials">
+                <ul>
+                    <li>
+                        <i class="bx bxs-star"></i>
+                    </li>
+                    <li>
+                        <i class="bx bxs-star"></i>
+                    </li>
+                    <li>
+                        <i class="bx bxs-star"></i>
+                    </li>
+                    <li>
+                        <i class="bx bxs-star"></i>
+                    </li>
+                    <li>
+                        <i class="bx bxs-star"></i>
+                    </li>
+                </ul>
+                <h3>Excellent hotel</h3>
+                <p>“Awesome yksum dolor sit ametco elit, sed do eiusmod tempor incididunt et md do eiusmoeiusmod tempor inte emamnsecacing eiusmoeiusmod”</p>
+                <div class="testimonials-content">
+                    <img src="<?php echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/testimonials/1.jpg" alt="Image">
+                    <h4>Ayman Jenis</h4>
+                    <span>CEO@Leasuely</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Testimonials Area -->
 
 <?php include_once 'views/template/footer-principal.php'; ?>
