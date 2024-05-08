971821d646e4ac9e5d4c295b790577041a271d75
napzalav <cesaralavila@hotmail.com>
2024-05-08 13:40:57 -0300

    update view contactos

--- views/principal/contactos/index.php
+++ views/principal/contactos/index.php
@@ -19,85 +19,99 @@
 </div>
 <!-- End Page Title Area -->
 
-<section class="our-rooms-area-two our-rooms-area-four ptb-100">
+<!-- Start Contact Area -->
+<section class="main-contact-area contact-info-area contact-info-three pt-100 pb-70">
     <div class="container">
         <div class="section-title">
-            <span>Nuestras habitaciones</span>
-            <h2>Fascinating rooms & suites</h2>
+            <span>Contact Us</span>
+            <h2>Drop us a message for any query</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque quibusdam deleniti porro praesentium. Aliquam minus quisquam velit in at nam.</p>
         </div>
-        <div class="tab industries-list-tab">
-            <div class="row">
-                <div class="col-lg-5">
-                    <div class="tabs row">
-                        <div class="col-lg-6 col-sm-6 single-tab">
-                            <div class="single-rooms">
-                                <i class="flaticon-online-booking"></i>
-                                <span class="booking-title">Free cost</span>
-                                <h3>No booking fee</h3>
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="contact-wrap contact-pages">
+                    <div class="contact-form contact-form-mb">
+                        <form id="contactForm">
+                            <div class="row">
+                                <div class="col-lg-6 col-sm-6">
+                                    <div class="form-group">
+                                        <input type="text" name="name" id="name" class="form-control" required data-error="Please enter your name" placeholder="Your Name">
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-6 col-sm-6">
+                                    <div class="form-group">
+                                        <input type="email" name="email" id="email" class="form-control" required data-error="Please enter your email" placeholder="Your Email">
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-6 col-sm-6">
+                                    <div class="form-group">
+                                        <input type="text" name="phone_number" id="phone_number" required data-error="Please enter your number" class="form-control" placeholder="Your Phone">
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-6 col-sm-6">
+                                    <div class="form-group">
+                                        <input type="text" name="msg_subject" id="msg_subject" class="form-control" required data-error="Please enter your subject" placeholder="Your Subject">
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-12 col-md-12">
+                                    <div class="form-group">
+                                        <textarea name="message" class="form-control textarea-hight" id="message" cols="30" rows="4" required data-error="Write your message" placeholder="Your Message"></textarea>
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-12 col-md-12">
+                                    <button type="submit" class="default-btn btn-two">
+                                        Send Message
+                                        <i class="flaticon-right"></i>
+                                    </button>
+                                    <div id="msgSubmit" class="h3 text-center hidden"></div>
+                                    <div class="clearfix"></div>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-lg-6 col-sm-6 single-tab">
-                            <div class="single-rooms">
-                                <i class="flaticon-podium"></i>
-                                <span class="booking-title">Free cost</span>
-                                <h3>Best rate guarantee</h3>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-sm-6 single-tab">
-                            <div class="single-rooms">
-                                <i class="flaticon-airport"></i>
-                                <span class="booking-title">Free cost</span>
-                                <h3>Reservations 24/7</h3>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-sm-6 single-tab">
-                            <div class="single-rooms">
-                                <i class="flaticon-slow"></i>
-                                <span class="booking-title">Free cost</span>
-                                <h3>High-speed Wi-Fi</h3>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-sm-6 single-tab">
-                            <div class="single-rooms">
-                                <i class="flaticon-coffee-cup-1"></i>
-                                <span class="booking-title">Free cost</span>
-                                <h3>Free breakfast</h3>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-sm-6 single-tab">
-                            <div class="single-rooms">
-                                <i class="flaticon-user"></i>
-                                <span class="booking-title">100% free</span>
-                                <h3>One person free</h3>
-                            </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
-                <div class="col-lg-7">
-                    <div class="tab_content">
-                        <div class="tabs_item">
-                            <div class="our-rooms-single-img room-bg-1">
-                            </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="row">
+                    <div class="col-lg-6 col-sm-6">
+                        <div class="single-contact-info">
+                            <i class="bx bx-envelope"></i>
+                            <h3>Email Us:</h3>
+                            <a href="mailto:hello@arduix.com">hello@ecorik.com</a>
+                            <a href="mailto:info@arduix.com">info@ecorik.com</a>
                         </div>
-                        <div class="tabs_item">
-                            <div class="our-rooms-single-img room-bg-2">
-                            </div>
+                    </div>
+                    <div class="col-lg-6 col-sm-6">
+                        <div class="single-contact-info">
+                            <i class="bx bx-phone-call"></i>
+                            <h3>Call Us:</h3>
+                            <a href="tel:+(123)1800-567-8990">Tel. + (123) 1800-567-8990</a>
+                            <a href="tel:+(124)1523-567-9874">Tel. + (124) 1523-567-9874</a>
                         </div>
-                        <div class="tabs_item">
-                            <div class="our-rooms-single-img room-bg-3">
-                            </div>
+                    </div>
+                    <div class="col-lg-6 col-sm-6">
+                        <div class="single-contact-info">
+                            <i class="bx bx-location-plus"></i>
+                            <h3>London</h3>
+                            <a href="#">205 Fida Walinton, Tongo Street Front The USA</a>
                         </div>
-                        <div class="tabs_item">
-                            <div class="our-rooms-single-img room-bg-4">
-                            </div>
-                        </div>
-                        <div class="tabs_item">
-                            <div class="our-rooms-single-img room-bg-5">
-                            </div>
-                        </div>
-                        <div class="tabs_item">
-                            <div class="our-rooms-single-img room-bg-6">
-                            </div>
+                    </div>
+                    <div class="col-lg-6 col-sm-6">
+                        <div class="single-contact-info">
+                            <i class="bx bx-phone-call"></i>
+                            <h3>Call Us:</h3>
+                            <a href="tel:+(123)1800-567-8990">Tel. + (123) 1800-567-8990</a>
+                            <a href="tel:+(124)1523-567-9874">Tel. + (124) 1523-567-9874</a>
                         </div>
                     </div>
                 </div>
@@ -105,6 +119,13 @@
         </div>
     </div>
 </section>
+<!-- End Contact Area -->
+
+<!-- Start Map Area -->
+<div class="map-area">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d905.538486924924!2d-65.41002327650922!3d-24.790181492850344!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x941bc3a366441101%3A0x17da4c2f2d3da126!2sHOTEL%20REGIDOR!5e0!3m2!1ses-419!2sar!4v1715186177131!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
+<!-- End Map Area -->
 
 
 <?php include_once 'views/template/footer-principal.php'; ?>
