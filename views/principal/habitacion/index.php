ec61e72a21b8697da17cbf0bc8e444b41237af27
napzalav <cesaralavila@hotmail.com>
2024-05-08 12:48:37 -0300

    add img habitaciones

--- views/principal/habitacion/index.php
+++ views/principal/habitacion/index.php
@@ -106,5 +106,237 @@
     </div>
 </section>
 
+<!-- Start Our Rooms Area -->
+<section class="our-rooms-area pb-100">
+    <div class="container">
+        <div class="section-title">
+            <span>Habitaciones</span>
+            <h2>Fascinantes habitaciones & suites</h2>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-sm-6">
+                <div class="single-rooms-three-wrap">
+                    <div class="single-rooms-three">
+                        <img src="<?php echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/rooms/5.jpg" alt="Image">
+                        <div class="single-rooms-three-content">
+                            <h3>Deluxe Room for Relax</h3>
+                            <ul class="rating">
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                            </ul>
+                            <span class="price">From $50.6/night</span>
+                            <a href="book-table.html" class="default-btn">
+                                Book Online
+                                <i class="flaticon-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="single-rooms-three-wrap">
+                    <div class="single-rooms-three">
+                        <img src="<?php echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/rooms/6.jpg" alt="Image">
+                        <div class="single-rooms-three-content">
+                            <h3>Double Room</h3>
+                            <ul class="rating">
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                            </ul>
+                            <span class="price">From $60.6/night</span>
+                            <a href="book-table.html" class="default-btn">
+                                Book Online
+                                <i class="flaticon-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="single-rooms-three-wrap">
+                    <div class="single-rooms-three">
+                        <img src="<?php echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/rooms/7.jpg" alt="Image">
+                        <div class="single-rooms-three-content">
+                            <h3>Trippe Bed Room</h3>
+                            <ul class="rating">
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                            </ul>
+                            <span class="price">From $50.6/night</span>
+                            <a href="book-table.html" class="default-btn">
+                                Book Online
+                                <i class="flaticon-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="single-rooms-three-wrap">
+                    <div class="single-rooms-three">
+                        <img src="<?php echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/rooms/8.jpg" alt="Image">
+                        <div class="single-rooms-three-content">
+                            <h3>Window Amenities Room</h3>
+                            <ul class="rating">
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                            </ul>
+                            <span class="price">From $50.6/night</span>
+                            <a href="book-table.html" class="default-btn">
+                                Book Online
+                                <i class="flaticon-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="single-rooms-three-wrap">
+                    <div class="single-rooms-three">
+                        <img src="<?php echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/rooms/9.jpg" alt="Image">
+                        <div class="single-rooms-three-content">
+                            <h3>The royal room</h3>
+                            <ul class="rating">
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                            </ul>
+                            <span class="price">From $50.6/night</span>
+                            <a href="book-table.html" class="default-btn">
+                                Book Online
+                                <i class="flaticon-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="single-rooms-three-wrap">
+                    <div class="single-rooms-three">
+                        <img src="<?php echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/rooms/10.jpg" alt="Image">
+                        <div class="single-rooms-three-content">
+                            <h3>Budget Room</h3>
+                            <ul class="rating">
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                                <li>
+                                    <i class="bx bxs-star"></i>
+                                </li>
+                            </ul>
+                            <span class="price">From $90.6/night</span>
+                            <a href="book-table.html" class="default-btn">
+                                Book Online
+                                <i class="flaticon-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-12">
+                <div class="page-navigation-area">
+                    <nav aria-label="Page navigation example text-center">
+                        <ul class="pagination">
+                            <li class="page-item">
+                                <a class="page-link page-links" href="#">
+                                    <i class='bx bx-chevrons-left'></i>
+                                </a>
+                            </li>
+                            <li class="page-item active">
+                                <a class="page-link" href="#">1</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">2</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">3</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">
+                                    <i class='bx bx-chevrons-right'></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Our Rooms Area -->
+
 
 <?php include_once 'views/template/footer-principal.php'; ?>
