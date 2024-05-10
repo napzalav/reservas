e128f8c21f650a87ba95461ebd06395009854b72
napzalav <cesaralavila@hotmail.com>
2024-05-10 17:16:23 -0300

    add js reservas

---
+++ views/principal/reservas.php
@@ -0,0 +1,13 @@
+<?php include_once 'views/template/header-principal.php'; ?>
+
+
+<div id='calendar'></div>
+
+
+<?php include_once 'views/template/footer-principal.php'; ?>
+
+<script src="<?php echo RUTA_PRINCIPAL . 'assets/principal/js/pages/reservas.js' ?>"></script>
+
+</body>
+
+</html>
