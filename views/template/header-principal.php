0a4e18fa0fdf56b7a0bf87efdebc0c2cde4c88d6
napzalav <cesaralavila@hotmail.com>
2024-05-17 17:02:06 -0300

    update header

--- views/template/header-principal.php
+++ views/template/header-principal.php
@@ -37,6 +37,10 @@
     <!-- Responsive CSS -->
     <link rel="stylesheet" href="<?php echo RUTA_PRINCIPAL . 'assets/principal'; ?>/css/responsive.css">
 
+    <!-- Estilo de Select2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
+
     <!-- Favicon -->
     <link rel="icon" type="image/png" href="<?php echo RUTA_PRINCIPAL . 'assets'; ?>/img/favicon.png">
 
